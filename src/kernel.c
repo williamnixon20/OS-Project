00eff5e5507d4ea76b8c828c58d28adf21185c19
william <williamnixon20@gmail.com>
2023-02-20 19:33:33 +0700

    [ADD] 3.7. Buffer

--- src/kernel.c
+++ src/kernel.c
@@ -6,8 +6,16 @@
 #include "lib-header/kernel_loader.h"
 
 void kernel_setup(void) {
-    uint32_t a;
-    uint32_t volatile b = 0x0000BABE;
-    __asm__("mov $0xCAFE0000, %0" : "=r"(a));
-    while (TRUE) b += 1;
+    // uint32_t a;
+    // uint32_t volatile b = 0x0000BABE;
+    // __asm__("mov $0xCAFE0000, %0" : "=r"(a));
+    // while (TRUE) b += 1;
+    framebuffer_clear();
+    framebuffer_write(6, 8,  'H', 0, 0xF);
+    framebuffer_write(6, 9,  'a', 0, 0xF);
+    framebuffer_write(6, 10, 'i', 0, 0xF);
+    framebuffer_write(6, 11, '!', 0, 0xF);
+    framebuffer_set_cursor(6, 11);
+    while (TRUE);
 }
+
