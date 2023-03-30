19107b90458481569e87bf81581122e1212468f6
william <williamnixon20@gmail.com>
2023-03-30 15:29:51 +0700

    [add] test checkpoint

--- src/kernel.c
+++ src/kernel.c
@@ -32,36 +32,41 @@ void kernel_setup(void)
     struct FAT32DriverRequest request = {
         .buf                   = cbuf,
         .name                  = "ikanaide",
-        .ext                   = "uwu",
+        .ext                   = "\0\0\0",
         .parent_cluster_number = ROOT_CLUSTER_NUMBER,
         .buffer_size           = 0,
     } ;
 
     write(request);  // Create folder "ikanaide"
-    memcpy(request.name, "kano1\0\0\0", 8);
-    write(request);  // Create folder "kano1"
-    memcpy(request.name, "ikanaide", 8);
+    request.parent_cluster_number = 3;
+    memcpy(request.name, "ikanaid2", 8);
     memcpy(request.ext, "\0\0\0", 3);
-    _delete(request); // Delete first folder, thus creating hole in FS
-
-    memcpy(request.name, "daijoubu", 8);
+    write(request);
+    request.parent_cluster_number = 4;
+    memcpy(request.ext, "uwu", 3);
     request.buffer_size = 5*CLUSTER_SIZE;
-    write(request);  // Create fragmented file "daijoubu"
+    write(request);
+    // delete(request);
+    // _delete(request); // Delete first folder, thus creating hole in FS
 
-    struct ClusterBuffer readcbuf;
-    read_clusters(&readcbuf, ROOT_CLUSTER_NUMBER+1, 1); 
-    // If read properly, readcbuf should filled with 'a'
+    // memcpy(request.name, "daijoubu", 8);
+    // request.buffer_size = 5*CLUSTER_SIZE;
+    // write(request);  // Create fragmented file "daijoubu"
 
-    request.buffer_size = CLUSTER_SIZE;
-    int resbabibu = read(request);   // Failed read due not enough buffer size
-    struct ClusterBuffer cbuf2[5];
-    for (uint32_t i = 0; i < 5; i++)
-        for (uint32_t j = 0; j < CLUSTER_SIZE; j++)
-            cbuf[i].buf[j] = 0;
-    request.buf = cbuf2;
-    request.buffer_size = 5*CLUSTER_SIZE;
-    int resbabibu2 = read(request);   // Success read on file "daijoubu"
-    resbabibu = resbabibu2 + resbabibu;
+    // struct ClusterBuffer readcbuf;
+    // read_clusters(&readcbuf, ROOT_CLUSTER_NUMBER+1, 1); 
+    // // If read properly, readcbuf should filled with 'a'
+
+    // request.buffer_size = CLUSTER_SIZE;
+    // int resbabibu = read(request);   // Failed read due not enough buffer size
+    // struct ClusterBuffer cbuf2[5];
+    // for (uint32_t i = 0; i < 5; i++)
+    //     for (uint32_t j = 0; j < CLUSTER_SIZE; j++)
+    //         cbuf[i].buf[j] = 0;
+    // request.buf = cbuf2;
+    // request.buffer_size = 5*CLUSTER_SIZE;
+    // int resbabibu2 = read(request);   // Success read on file "daijoubu"
+    // resbabibu = resbabibu2 + resbabibu;
 
   activate_keyboard_interrupt();
   while (TRUE)
