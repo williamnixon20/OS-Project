12c0997c3a1900ba7994ea685f936af8fdbe6790
NicholasLiem <13521135@std.stei.itb.ac.id>
2023-04-22 13:53:37 +0700

    update: QEMU Bootloop..? Probably because TSS GDT Struct

--- src/gdt.c
+++ src/gdt.c
@@ -54,7 +54,7 @@ struct GlobalDescriptorTable global_descriptor_table = {
             GRANULARITY,
 
             BASE
-        },
+        }, // Kernel Code Descriptor
         {
             (uint16_t) LIMIT,
             
@@ -74,100 +74,49 @@ struct GlobalDescriptorTable global_descriptor_table = {
             GRANULARITY,
 
             BASE
-        }
-    }
-};
-
-/**
- * _gdt_gdtr, predefined system GDTR. 
- * GDT pointed by this variable is already set to point global_descriptor_table above.
- * From: https://wiki.osdev.org/Global_Descriptor_Table, GDTR.size is GDT size minus 1.
- */
-struct GDTR _gdt_gdtr = {
-    sizeof(global_descriptor_table), &global_descriptor_table
-};
-
-// Referensinya ada di sini https://wiki.osdev.org/GDT_Tutorial
-
-static struct GlobalDescriptorTable global_descriptor_table = {
-    .table = {
-        {
-            .segment_low       = 0,
-            .base_low          = 0,
-            .base_mid          = 0,
-            .type_bit          = 0,
-            .non_system        = 0,
-            .privilege_level   = 0,
-            .segment_present   = 0,
-            .segment_limit     = 0,
-            .available_sys     = 0,
-            .bitsegment_64     = 0,
-            .default_opsize    = 0,
-            .granularity       = 0,
-            .base_high         = 0
-        }, // Null Descriptor
-        {
-            .segment_low       = 0xFFFF,
-            .base_low          = 0x0000,
-            .base_mid          = 0x00,
-            .type_bit          = 0xA,  // Code Segment (Executable, Readable)
-            .non_system        = 1,    // Non-system segment
-            .privilege_level   = 0,    // Ring 0
-            .segment_present   = 1,    // Segment is present
-            .segment_limit     = 0xFFFF,
-            .available_sys     = 0,
-            .bitsegment_64     = 1,    // 64-bit segment
-            .default_opsize    = 0,    // 16-bit operands
-            .granularity       = 1,    // Segment limit scaled by 4K
-            .base_high         = 0x00
-        }, // Kernel Code Descriptor
-        {
-            .segment_low       = 0xFFFF,
-            .base_low          = 0x0000,
-            .base_mid          = 0x00,
-            .type_bit          = 0x2,  // Data Segment (Writable)
-            .non_system        = 1,    // Non-system segment
-            .privilege_level   = 0,    // Ring 0
-            .segment_present   = 1,    // Segment is present
-            .segment_limit     = 0xFFFF,
-            .available_sys     = 0,
-            .bitsegment_64     = 1,    // 64-bit segment
-            .default_opsize    = 0,    // 16-bit operands
-            .granularity       = 1,    // Segment limit scaled by 4K
-            .base_high         = 0x00
         }, // Kernel Data Descriptor
         {
-            .segment_low       = 0xFFFF,
-            .base_low          = 0x0000,
-            .base_mid          = 0x00,
-            .type_bit          = 0xB,  // Code Segment (Executable, Readable)
-            .non_system        = 1,    // Non-system segment
-            .privilege_level   = 3,    // Ring 3
-            .segment_present   = 1,    // Segment is present
-            .segment_limit     = 0xFFFF,
-            .available_sys     = 0,
-            .bitsegment_64     = 1,    // 64-bit segment
-            .default_opsize    = 0,    // 16-bit operands
-            .granularity       = 1,    // Segment limit scaled by 4K
-            .base_high         = 0x00
+            (uint16_t) LIMIT,
+
+            BASE,  
+            BASE,
+
+            TYPE_CODE,
+            TYPE_BIT,
+            0x3, // Privilege nya diubah
+            PRESENT,
+
+            (char) LIMIT,
+
+            NULL,
+            NULL,
+            OP_SIZE,
+            GRANULARITY,
+
+            BASE
         }, // User Code Descriptor
         {
-            .segment_low       = 0xFFFF,
-            .base_low          = 0x0000,
-            .base_mid          = 0x00,
-            .type_bit          = 0x2,   // Type: Data, Writable, Non-Expandable
-            .non_system        = 1,     // S bit
-            .privilege_level   = 3,     // DPL
-            .segment_present   = 1,     // P bit
-            .segment_limit     = 0xF,   // Limit: 4KB
-            .available_sys     = 0,
-            .bitsegment_64     = 0,
-            .default_opsize    = 1,     // Default operand size is 32 bits
-            .granularity       = 1,     // Granularity: 4KB
-            .base_high         = 0x00
-        },
+            (uint16_t) LIMIT,
+            
+            BASE,
+            BASE,
+
+            TYPE_DATA,
+            TYPE_BIT,
+            0x3,
+            PRESENT,
+
+            (char) LIMIT,
+
+            NULL,
+            NULL,
+            OP_SIZE,
+            GRANULARITY,
+
+            BASE
+        }, // User Data Descriptor
         {
-            .segment_limit      = (sizeof(struct TSSEntry) & (0xF << 16)) >> 16,
+            // .segment_high      = (sizeof(struct TSSEntry) & (0xF << 16)) >> 16,
             .segment_low       = sizeof(struct TSSEntry),
             .base_high         = 0,
             .base_mid          = 0,
@@ -184,6 +133,15 @@ static struct GlobalDescriptorTable global_descriptor_table = {
         },
         {0}
     }
+};
+
+/**
+ * _gdt_gdtr, predefined system GDTR. 
+ * GDT pointed by this variable is already set to point global_descriptor_table above.
+ * From: https://wiki.osdev.org/Global_Descriptor_Table, GDTR.size is GDT size minus 1.
+ */
+struct GDTR _gdt_gdtr = {
+    sizeof(global_descriptor_table), &global_descriptor_table
 };
 
 void gdt_install_tss(void) {
