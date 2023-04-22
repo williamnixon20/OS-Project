b5845dfc0df787d257f60faaad3c26dadd7268f2
NicholasLiem <13521135@std.stei.itb.ac.id>
2023-04-22 19:07:40 +0700

    feat: editting idt.c, interrupt.c, ([-] implementation of puts)

--- src/interrupt/idt.c
+++ src/interrupt/idt.c
@@ -20,8 +20,11 @@ void initialize_idt(void) {
    * Segment: GDT_KERNEL_CODE_SEGMENT_SELECTOR
    * Privilege: 0
    */
-    for (uint8_t i = 0; i < ISR_STUB_TABLE_LIMIT; i++) {
-        set_interrupt_gate(i, isr_stub_table[i], GDT_KERNEL_CODE_SEGMENT_SELECTOR, 0);
+    // for (uint8_t i = 0; i < ISR_STUB_TABLE_LIMIT; i++) {
+    //     set_interrupt_gate(i, isr_stub_table[i], GDT_KERNEL_CODE_SEGMENT_SELECTOR, 0);
+    // }
+    for (uint8_t i = 0x30; i <= 0x3F; ++i) {
+        set_interrupt_gate(i, isr_stub_table[i], GDT_KERNEL_CODE_SEGMENT_SELECTOR, 0x3);
     }
     
     __asm__ volatile("lidt %0" : : "m"(_idt_idtr));
--- src/interrupt/interrupt.c
+++ src/interrupt/interrupt.c
@@ -3,25 +3,36 @@
 #include "../keyboard/keyboard.h"
 
 struct TSSEntry _interrupt_tss_entry = {
-    .prev_tss = 0,
-    .esp0 = 0,
-    .ss0 = 0,
-    .unused_register = {0}
+    .ss0  = GDT_KERNEL_DATA_SEGMENT_SELECTOR,
 };
 
-
-
-void main_interrupt_handler(
-    __attribute__((unused)) struct CPURegister cpu,
-    uint32_t int_number,
-    __attribute__((unused)) struct InterruptStack info
-) {
+void main_interrupt_handler(struct CPURegister cpu, uint32_t int_number, struct InterruptStack info) {
     switch (int_number) {
-        case 0x21:
+        case 0x21: // Atau PIC1_OFFSET + IRQ_KEYBOARD
             keyboard_isr();
+            break;
+        case 0x30:
+            syscall(cpu, info);
             break;
     }
 }
+
+void syscall(struct CPURegister cpu, __attribute__((unused)) struct InterruptStack info) {
+    if (cpu.eax == 0) {
+        struct FAT32DriverRequest request = *(struct FAT32DriverRequest*) cpu.ebx;
+        *((int8_t*) cpu.ecx) = read(request);
+    } else if (cpu.eax == 4) {
+        keyboard_state_activate();
+        __asm__("sti"); // Due IRQ is disabled when main_interrupt_handler() called
+        while (is_keyboard_blocking());
+        char buf[KEYBOARD_BUFFER_SIZE];
+        get_keyboard_buffer(buf);
+        memcpy((char *) cpu.ebx, buf, cpu.ecx);
+    } else if (cpu.eax == 5) {
+        puts((char *) cpu.ebx, cpu.ecx, cpu.edx); // Modified puts() on kernel side
+    }
+}
+
 
 void io_wait(void) {
     out(0x80, 0);
