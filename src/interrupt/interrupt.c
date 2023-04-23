89489d5a0f156ff8bced725e876b7f28640c3cb5
Kandida Gunawangit config --global user.name Kandida <kandidagunawan@gmail.com>
2023-04-23 11:06:57 +0700

    edit privilege  & init interrupt handler

--- src/interrupt/interrupt.c
+++ src/interrupt/interrupt.c
@@ -1,47 +1,85 @@
 #include "interrupt.h"
+#include "idt.h"
 #include "../lib-header/portio.h"
 #include "../keyboard/keyboard.h"
 
 struct TSSEntry _interrupt_tss_entry = {
     .prev_tss = 0,
     .esp0 = 0,
-    .ss0 = 0,
-    .unused_register = {0}
-};
-
-
+    // .ss0 = 0,
+    .ss0 = GDT_KERNEL_DATA_SEGMENT_SELECTOR,
+    .unused_register = {0}};
 
 void main_interrupt_handler(
     __attribute__((unused)) struct CPURegister cpu,
     uint32_t int_number,
-    __attribute__((unused)) struct InterruptStack info
-) {
-    switch (int_number) {
-        case 0x21:
-            keyboard_isr();
-            break;
+    __attribute__((unused)) struct InterruptStack info)
+{
+    switch (int_number)
+    {
+    case 0x21:
+        keyboard_isr();
+        break;
+    }
+}
+void main_interrupt_handler(struct CPURegister cpu, uint32_t int_number, struct InterruptStack info)
+{
+    switch (int_number)
+    {
+    case PIC1_OFFSET + IRQ_KEYBOARD:
+        keyboard_isr();
+        break;
+    case 0x30:
+        syscall(cpu, info);
+        break;
     }
 }
 
-void io_wait(void) {
+void syscall(struct CPURegister cpu, __attribute__((unused)) struct InterruptStack info)
+{
+    if (cpu.eax == 0)
+    {
+        struct FAT32DriverRequest request = *(struct FAT32DriverRequest *)cpu.ebx;
+        *((int8_t *)cpu.ecx) = read(request);
+    }
+    else if (cpu.eax == 4)
+    {
+        keyboard_state_activate();
+        __asm__("sti"); // Due IRQ is disabled when main_interrupt_handler() called
+        while (is_keyboard_blocking())
+            ;
+        char buf[KEYBOARD_BUFFER_SIZE];
+        get_keyboard_buffer(buf);
+        memcpy((char *)cpu.ebx, buf, cpu.ecx);
+    }
+    else if (cpu.eax == 5)
+    {
+        puts((char *)cpu.ebx, cpu.ecx, cpu.edx); // Modified puts() on kernel side
+    }
+}
+
+void io_wait(void)
+{
     out(0x80, 0);
 }
 
-void pic_ack(uint8_t irq) {
+void pic_ack(uint8_t irq)
+{
     if (irq >= 8)
         out(PIC2_COMMAND, PIC_ACK);
     out(PIC1_COMMAND, PIC_ACK);
 }
 
-void pic_remap(void) {
+void pic_remap(void)
+{
     uint8_t a1, a2;
 
     // Save masks
-    a1 = in(PIC1_DATA); 
+    a1 = in(PIC1_DATA);
     a2 = in(PIC2_DATA);
 
     // Starts the initialization sequence in cascade mode
-    out(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4); 
+    out(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);
     io_wait();
     out(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
     io_wait();
@@ -49,9 +87,9 @@ void pic_remap(void) {
     io_wait();
     out(PIC2_DATA, PIC2_OFFSET); // ICW2: Slave PIC vector offset
     io_wait();
-    out(PIC1_DATA, 0b0100);      // ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
+    out(PIC1_DATA, 0b0100); // ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
     io_wait();
-    out(PIC2_DATA, 0b0010);      // ICW3: tell Slave PIC its cascade identity (0000 0010)
+    out(PIC2_DATA, 0b0010); // ICW3: tell Slave PIC its cascade identity (0000 0010)
     io_wait();
 
     out(PIC1_DATA, ICW4_8086);
@@ -64,15 +102,19 @@ void pic_remap(void) {
     out(PIC2_DATA, a2);
 }
 
-void activate_keyboard_interrupt(void) {
+void activate_keyboard_interrupt(void)
+{
     out(PIC1_DATA, PIC_DISABLE_ALL_MASK ^ (1 << IRQ_KEYBOARD));
     out(PIC2_DATA, PIC_DISABLE_ALL_MASK);
 }
 
-void set_tss_kernel_current_stack(void) {
+void set_tss_kernel_current_stack(void)
+{
     uint32_t stack_ptr;
     // Reading base stack frame instead esp
-    __asm__ volatile ("mov %%ebp, %0": "=r"(stack_ptr) : /* <Empty> */);
+    __asm__ volatile("mov %%ebp, %0"
+                     : "=r"(stack_ptr)
+                     : /* <Empty> */);
     // Add 8 because 4 for ret address and other 4 is for stack_ptr variable
-    _interrupt_tss_entry.esp0 = stack_ptr + 8; 
+    _interrupt_tss_entry.esp0 = stack_ptr + 8;
 }
